f5e17ecec6a7d552cfd4d09186f8b1c7ba810f78
sourabh09 <qa.sourabh@gmail.com>
2020-10-26 09:12:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+     var mapProp= {
+  center:new google.maps.LatLng(51.508742,-0.120850),
+  zoom:5,
+};
+    var map = new google.maps.Map(document.getElementById("map"),mapProp);
+   
+});
